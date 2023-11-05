c99fa4b1d29ae4a0a548e1f008b109a906933a13
Okiki Ojo <okikioluwa.ojo@ontariotechu.net>
2023-11-05 22:18:39 +0000

    feat: add seed data for permissions, roles, and slugs

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+dotenv.config({
+    path: ".env.local"
+})
+
+export const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } =  process.env;
+export const PGPORT = +(process.env.PGPORT ?? 5432);
