4c6bbb504de46f373566cbf76ff8b80143f86b58
John Howe <johnathan.howe@ontariotechu.net>
2023-11-15 16:48:20 +0000

    make default page files

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,7 @@
-import { ListUsers } from "@/components/list-users";
-import { getUsers } from "@/lib/actions";
-
-const initialList = await getUsers();
 export default function Home() {
   return (
     <div>
-      <ListUsers initialList={initialList} />
+      <p>test</p>
     </div>
   );
 }
