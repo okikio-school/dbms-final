a0ae68c4763ce4697fd6091ad7e061ebb36b0ad1
Okiki Ojo <okikioluwa.ojo@ontariotechu.net>
2023-11-17 08:03:28 +0000

    feat: add search & edtor pages

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,13 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Home() {
   return (
     <div>
-      <p>search</p>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="search">Search</Label>
+        <Input type="text" id="search" placeholder="Search for posts..." />
+      </div>
     </div>
   );
 }
