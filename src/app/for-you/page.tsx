d1ad141dd6775e8173cdc692f4fec0bcc5fb4726
John Howe <94157812+johnh-otu@users.noreply.github.com>
2023-11-22 18:36:45 -0500

    usesession implemented

--- src/app/for-you/page.tsx
+++ src/app/for-you/page.tsx
@@ -2,8 +2,14 @@
 import { getPosts, getRelevantPosts } from "@/lib/actions.ts";
 import { useRouter } from "next/navigation";
 import { PostsList } from "@/components/foryou-list";
+import { getServerSession, type AuthOptions } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/_options";
 
 const userID = "6133925e-359f-491e-bb0c-fab9e6c090e8";
+
+const usersession = await getServerSession(authOptions as unknown as AuthOptions)
+const userid2 = usersession?.user.id;
+
 const initialFeed = await getRelevantPosts(userID);
 
 export default async function ForYouPage() {
